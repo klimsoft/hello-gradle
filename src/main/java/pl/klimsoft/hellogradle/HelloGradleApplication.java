23b2be3ec499dd15b4db5a4783fc2dad736a6a36
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2020-12-12 10:53:13 +0100

    First version

---
+++ src/main/java/pl/klimsoft/hellogradle/HelloGradleApplication.java
@@ -0,0 +1,11 @@
+package pl.klimsoft.hellogradle;
+
+import java.time.LocalDate;
+
+public class HelloGradleApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Gradle at " + LocalDate.now());
+	}
+
+}
---
+++ src/test/java/pl/klimsoft/hellogradle/HelloGradleApplicationTests.java
@@ -0,0 +1,14 @@
+package pl.klimsoft.hellogradle;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HelloGradleApplicationTests {
+
+	@Test
+	void sampleTest() {
+		int result = 2 + 2;
+		Assertions.assertEquals(4, 4);
+	}
+
+}
