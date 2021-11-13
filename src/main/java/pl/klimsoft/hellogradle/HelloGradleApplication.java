fc5bedeab39d339ae157fd595cb6f3de1620958f
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2021-11-13 23:12:20 +0100

    fixed date time

--- src/main/java/pl/klimsoft/hellogradle/HelloGradleApplication.java
+++ src/main/java/pl/klimsoft/hellogradle/HelloGradleApplication.java
@@ -1,11 +1,11 @@
 package pl.klimsoft.hellogradle;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class HelloGradleApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Gradle at " + LocalDate.now());
+		System.out.println("Hello Gradle at " + LocalDateTime.now());
 	}
 
 }
